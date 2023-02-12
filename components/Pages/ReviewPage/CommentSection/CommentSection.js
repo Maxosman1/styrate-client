3cfbe33c9227ff5029a2e6266a4dcf5224684505
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-02-12 20:42:53 +0000

    added comment pull logic

--- components/Pages/ReviewPage/CommentSection/CommentSection.js
+++ components/Pages/ReviewPage/CommentSection/CommentSection.js
@@ -1,11 +1,44 @@
 import { CommentSectionContainer } from "./CommentSection.styled";
+import {db} from '../../../../firebase/firebase'
+import { useEffect, useState } from "react";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
 
 const CommentSection = () => {
+    const [comments, setComments] = useState(null)
+    const dbRef = collection(db, 'comments')
+    useEffect(()=>{
+        fetchComments()
+    },[])
+    const fetchComments = async() => {
+        const q = query(dbRef, orderBy('createdOn', 'desc'))
+        try{
+            const rawData = await getDocs(q);
+            const result = rawData.docs.map(doc => ({
+                commentID: doc.id,
+                username: doc.data().username,
+                commentText: doc.data().commentText,
+                createdOn: doc.data().createdOn,
+            })) 
+            if(result.length!=0){
+                setComments(result)
+            }
+        }catch(e){}
+    }
     return (
         <CommentSectionContainer>
             <h3>Comments</h3>
             <div className="commentList">
-                
+            {
+                (comments)
+                ? (
+                    comments.map(comment =>(
+                        <div className="comment" key={comment.commentID}>
+                            {comment.commentText}
+                        </div>
+                    ))
+                )
+                : <>Loading</>
+            }
             </div>
         </CommentSectionContainer>
     );
