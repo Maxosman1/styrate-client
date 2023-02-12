f99d9d8471766a1ae553532717caa81565c73a38
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-02-12 13:57:46 +0000

    init for review page

---
+++ components/Pages/ReviewPage/ReviewPage.js
@@ -0,0 +1,11 @@
+import { ReviewListContainer } from "../Reviews/ReviewList/ReviewList.styled";
+
+const ReviewPage = ({reviewData}) => {
+    return (
+        <ReviewListContainer>
+
+        </ReviewListContainer>
+    );
+}
+
+export default ReviewPage;
---
+++ src/pages/review/[reviewID].js
@@ -0,0 +1,49 @@
+import ReviewPage from 'components/Pages/ReviewPage/ReviewPage';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import Head from 'next/head';
+import {db}  from '../../../firebase/firebase'
+
+const Review = ({reviewData}) => {
+    return (
+        <>
+        <Head>
+          <title>Styrate | {reviewData.postID}</title>
+          <meta name="description" content="[Insert Descritption]" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <ReviewPage reviewData={reviewData}/>
+      </>
+    );
+}
+export async function getServerSideProps(context) {
+    const postID = context.params.reviewID
+    const q = query(collection(db, "reviews"), where('__name__', "==", postID));
+    const rawData = await getDocs(q);
+    const reviewData = rawData.docs.map(doc => ({
+        reviewID: doc.id,
+        tiktokVideoId: doc.data().tiktokVideoId,
+        amazonProductLink: doc.data().amazonProductLink,
+        username: doc.data().username,
+        textReview: doc.data().textReview,
+        productType: doc.data().productType,
+        upvotes: doc.data().upvotes,
+        // createdOn: doc.data().createdOn
+    })) 
+    if(reviewData.length===0){
+      return {
+        redirect: {
+          permanent: false,
+          destination: "/404",
+        }
+      }
+    }else{
+      return{
+        props:{
+          reviewData: reviewData
+        }
+      }
+    }
+  }
+    
+export default Review;
