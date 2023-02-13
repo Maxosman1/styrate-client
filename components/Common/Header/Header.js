5d3174ff2a503cb4b84d326bf00db34e30261895
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-02-13 00:50:45 +0000

    basic header

--- components/Common/Header/Header.js
+++ components/Common/Header/Header.js
@@ -1,9 +1,16 @@
+import Link from "next/link";
 import { HeaderContainer } from "./Header.styled";
 
 const Header = () => {
     return (
-        <HeaderContainer>
-            Header
+        <HeaderContainer className="center">
+            <nav>
+                <ul>
+                    <li><Link href="/"><h2>Styrate</h2></Link></li>
+                    <li><Link href="/reviews">Reviews</Link></li>
+                    <li><Link href="/">Create</Link></li>
+                </ul>
+            </nav>
         </HeaderContainer>
     );
 }
--- components/Common/Header/Header.styled.js
+++ components/Common/Header/Header.styled.js
@@ -2,4 +2,33 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
     width: 100%;
+    height: 60px;
+    background-color: #EEEEEE;
+    box-shadow: 0 0 5px #BBBBBB;
+    & nav{
+        width: 95%; height: 100%;
+        & ul{
+            display: flex;
+            align-items: center;
+            height: 100%;
+            & li{
+                height: 100%;
+                display: flex; align-items: center; justify-content: center;
+                & > *{
+                    width: 100px;
+                    height: 100%;
+                    outline: unset;
+                    display: flex; align-items: center; justify-content: center;
+                    padding: 0 10px;
+                    &:hover{
+                        background-color: #BBBBBB;
+                    }
+                }
+                & h2{
+                    font-size: 1.3em;
+                    font-weight: 600;
+                }
+            }
+        }
+    }
 `
